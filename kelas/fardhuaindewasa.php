b0f909f0effe1678ed6efa98cf0d9c6d2ce4c198
akqan <146180727+akqan@users.noreply.github.com>
2023-09-27 17:42:37 +0800

    Add files via upload

---
+++ kelas/fardhuaindewasa.php
@@ -0,0 +1,120 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>AKQAN | KELAS FARDHU AIN (DEWASA)</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	</head>
+	<div class="bg">
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+					    <div class="inner">
+					        <!-- Logo -->
+					        <a href="../index.php" class="logo">
+					            <img src="../images/logo.png" alt="Your Logo Alt Text">
+					            <span class="title">AKADEMI AL-QURAN AN-NUR (AKQAN)</span>
+					        </a>
+
+					        <!-- Nav -->
+					        <nav>
+					            <ul>
+					                <li><a href="#menu">Menu</a></li>
+					            </ul>
+					        </nav>
+					    </div>
+					</header>
+
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="../index.php">Utama</a></li>
+
+							<li><a href="../kelas.php" class="active">Kelas Pengajian</a></li>
+
+							<li><a href="../program.php">Program</a></li>
+
+							<li>
+								<a href="#" class="dropdown-toggle">AKQAN</a>
+
+								<ul>
+									<li><a href="../info.php">Tentang Kami</a></li>
+									<li><a href="../organisasi.php">Carta Organisasi</a></li>
+								</ul>
+							</li>
+							<li><a href="../contact.php">Hubungi Kami</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<h1>KELAS FARDHU AIN (DEWASA)</h1>
+
+							<div class="image main">
+								<a href="../images/kelas/fd/fd.jpg" target="blank"><img src="../images/kelas/fd/fd.jpg" class="img-fluid" alt="" /></a>
+							</div>
+
+							<p>
+								Kami di <b>AKQAN</b> menyediakan <b>pembelajaran</b> dengan mementingkan <b>5 perkara</b> ini:
+								<ol>
+									<li>✅ Tenaga pengajar yang <b>terlatih dan berpengalaman</b>.</li>
+									<li>✅ Sistem pembelajaran yang <b>berkaedah dan bersanad</b>.</li>
+									<li>✅ Tempat belajar yang <b>kondusif dan berhawa dingin</b>.</li>
+									<li>✅ Kekerapan pembelajaran <b>20 kali sebulan  (termasuk kelas eksklusif)</b>.</li>
+									<li>✅ Yuran komitmen yang <b>murah dan sangat berpatutan</b>.</li>
+								</ol>
+
+							❗ <b>Tempat terhad</b> ❗<br><br>
+
+							<b>Daftar segera dan tempah tempat anda hari ini</b>.<br><br>
+
+							Untuk <b>pendaftaran</b> atau sebarang <b>pertanyaan</b> boleh terus berhubung dengan kami di talian : <a href="https://wa.me/601170194238" target="blank"><b>011-7019 4238</b></a><br><br>
+
+							<b>“Betulkan Bacaanmu, Sempurnakan Ibadahmu”</b><br><br></p>
+							
+							<a href="../kelas.php"><input type="button" value="Kembali" class="primary"/></a>
+
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<ul class="icons">
+									<li><a href="https://wa.me/601170194238" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon style2 fa-linkedin"><span class="label">LinkedIn</span></a></li>
+								</ul>
+
+								&nbsp;
+							</section>
+
+							<ul class="copyright">
+								<li>Copyright © 2020 Company Name </li>
+								<li>Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../assets/js/main.js"></script>
+	</body>
+</html>
